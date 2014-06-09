1ad729ea1fb4d01938cbe7a0f544a25a07b37799
H.Y. Leung <hy.leung@gmail.com>
2014-06-09 14:26:53 -0700

    Implements check for registration id

--- gcm-client/app/src/main/java/com/demo/gcmClient/app/MainActivity.java
+++ gcm-client/app/src/main/java/com/demo/gcmClient/app/MainActivity.java
@@ -1,5 +1,9 @@
 package com.demo.gcmClient.app;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,19 +12,35 @@ import android.view.MenuItem;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String SENDER_ID = "4524831114";
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public static final String TAG = "GCM_DEMO";
+    public static final String PROPERTY_REG_ID = "GCM_REGISTRATION_ID";
+    public static final String PROPERTY_APP_VERSION = "APP_VERSION";
+    private GoogleCloudMessaging gcm;
+    private String registrationId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if(checkPlayServices()) {
             //continue with the setup
+            gcm = GoogleCloudMessaging.getInstance(this);
+            registrationId = getRegistrationId(this);
+            if(registrationId.isEmpty()) {
+                //register in the background
+                registerInBackground();
+            }
         }
+    }
+
+    private void registerInBackground() {
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -56,12 +76,42 @@ public class MainActivity extends ActionBarActivity {
                 GooglePlayServicesUtil.getErrorDialog(resultCode,this,PLAY_SERVICES_RESOLUTION_REQUEST).show();
             }
             else {
-                Log.e("GCM_DEMO","This device is not supported");
+                Log.e(TAG,"This device is not supported");
                 finish();
             }
             return false;
         }
         return true;
     }
+    private String getRegistrationId(Context context) {
+        final SharedPreferences preferences = getGcmSharedPreferences(context);
+        String registrationId = preferences.getString(PROPERTY_REG_ID,"");
+        if(registrationId.isEmpty()) {
+            Log.i(TAG,"Registration not found");
+            return "";
+        }
+        int registeredVersion = preferences.getInt(PROPERTY_APP_VERSION,Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if(registeredVersion!=currentVersion) {
+            Log.i(TAG,String.format("Registered app version (%d), doesn't match the current app version (%d",registeredVersion,currentVersion));
+            return "";
+        }
+        return registrationId;
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(),0);
+            return packageInfo.versionCode;
+        }
+        catch(PackageManager.NameNotFoundException nameNotFound) {
+            throw new RuntimeException("Could not find package name",nameNotFound);
+        }
+    }
+
+    private SharedPreferences getGcmSharedPreferences(Context context) {
+        return getSharedPreferences(MainActivity.class.getSimpleName(), MODE_PRIVATE);
+    }
+
 
 }
