d49431197685f29524f5aa3d85faf6515d7bcc78
H.Y. Leung <hy.leung@gmail.com>
2014-06-09 12:52:11 -0700

    Adds check for Play Services.

--- gcm-client/app/src/main/java/com/demo/gcmClient/app/MainActivity.java
+++ gcm-client/app/src/main/java/com/demo/gcmClient/app/MainActivity.java
@@ -2,18 +2,32 @@ package com.demo.gcmClient.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public static final String SENDER_ID = "4524831114";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(checkPlayServices()) {
+            //continue with the setup
+        }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,4 +47,21 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        Log.d("GCM_DEMO",String.format("PlayServices availability check: %d",resultCode));
+        if(resultCode!= ConnectionResult.SUCCESS) {
+            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode,this,PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }
+            else {
+                Log.e("GCM_DEMO","This device is not supported");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
 }
